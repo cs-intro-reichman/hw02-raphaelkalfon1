befe28507e3dc376e617ffb518398a314b7620ba
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-02 22:37:23 +0200

    Update OneOfEachStats.java

--- OneOfEachStats.java
+++ OneOfEachStats.java
@@ -1,24 +1,11 @@
-public class OneOfEach {
-    public static void main(String[] args) {
-        int boys = 0;
-        int girls = 0;
-
-        while (boys == 0 || girls == 0) {
-            // probability for a boy or a girl
-            if (Math.random() < 0.5) {
-                System.out.print("b ");
-                boys++;
-            } else {
-                System.out.print("g ");
-                girls++;
-            }
-
-        }
-                // go to the line with
-                System.out.println();
-
-         int totalofChildren = boys + girls ;
-
-        System.out.println("You made it... and you now have " + totalofChildren + " children.");
-    }
+/**
+ *  Computes some statistics about families in which the parents decide 
+ *  to have children until they have at least one child of each gender.
+ *  The program expects to get one command-line argument: an int value
+ * 	that determines how many families to simulate.
+ */
+public class OneOfEachStats1 {
+	public static void main (String[] args) {
+		//// Put your code here
+	}
 }
