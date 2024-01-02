4f5ad64b30e592ae15a8d41dee2e3f4d99c4a8da
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-02 14:34:55 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,26 +1,24 @@
 public class DamkaBoard {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Please provide an integer command-line argument.");
+            System.out.println("no");
             return;
         }
-	    
-        int x = Integer.parseInt(args[0]); // Set the number of rows and columns
+
+        int x = Integer.parseInt(args[0]); 
 
         for (int i = 0; i < x; i++) {
+           
             if (i % 2 == 1) {
                 System.out.print(" ");
-	        // Add a space at the beginning every second line
             }
 
             
             for (int j = 0; j < x; j++) {
                 System.out.print("* ");
-	        // Print "* " line with length x
             }
 
             System.out.println(); 
-	    // Move to the next line
         }
     }
 }
