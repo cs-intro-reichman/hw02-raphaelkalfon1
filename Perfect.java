f765c53053c89038b457b42c0f71fe61c240778c
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-02 13:01:06 +0200

    Update Perfect.java

--- Perfect.java
+++ Perfect.java
@@ -1,16 +1,19 @@
 public class Perfect {
     public static void main(String[] args) {
-        int N = Integer.parseInt(args[0]);
-        int sum = 0;
 
-        for (int i = 1; i < N; i++) {
+        int N = Integer.parseInt(args[0]);
+        int sum = 1;  // First divisor is 1
+        String divisors = "1";
+
+        for (int i = 2; i <= N / 2; i++) {
             if (N % i == 0) {
                 sum += i;
+                divisors += " + " + i;
             }
         }
 
         if (sum == N) {
-            System.out.println(N + " is a perfect number since " + N + " = " + sum);
+            System.out.println(N + " is a perfect number since " + N + " = " + divisors);
         } else {
             System.out.println(N + " is not a perfect number");
         }
