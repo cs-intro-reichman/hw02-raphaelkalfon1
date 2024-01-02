39f91332600a7ad928cde29f1b0a7195857426ef
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-02 15:02:36 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,26 +1,27 @@
 public class DamkaBoard {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println(« no. »);
+            System.out.println("no");
             return;
         }
 
         int x = Integer.parseInt(args[0]);
 
         for (int i = 0; i < x; i++) {
-            if (i == 0) {
-                System.out.print(" ");
-            }
-
-            if (i > 0 && i % 2 == 1) {
+            if (i % 2 == 1) {
                 System.out.print(" ");
             }
 
             for (int j = 0; j < x; j++) {
                 System.out.print("*");
+
                 if (j < x - 1) {
-                    System.out.print(" ");
+                    System.out.print(" "); 
                 }
+            }
+
+            if (i % 2 == 0 && i < x - 1) {
+                System.out.print(" ");
             }
 
             System.out.println();
