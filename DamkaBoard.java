b482781c2567418b909266c3c4972178f46878ef
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-02 14:52:38 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,7 +1,7 @@
 public class DamkaBoard {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println(« no »);
+            System.out.println(« no. »);
             return;
         }
 
