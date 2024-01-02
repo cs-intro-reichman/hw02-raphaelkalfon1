47bd91aeb7c9c66de62956a93d218e79c31e4c51
raphaelkalfon1 <154341557+raphaelkalfon1@users.noreply.github.com>
2024-01-02 15:12:06 +0200

    Update DamkaBoard.java

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -1,29 +1,28 @@
 public class DamkaBoard {
     public static void main(String[] args) {
-        if (args.length == 0) {
-            System.out.println("no");
-            return;
-        }
-
+        \\ Size of the board 
         int x = Integer.parseInt(args[0]);
 
         for (int i = 0; i < x; i++) {
+            // Add a space at the beginning of odd-numbered rows
             if (i % 2 == 1) {
                 System.out.print(" ");
             }
 
             for (int j = 0; j < x; j++) {
+                // Print an asterisk
                 System.out.print("*");
-
+                // Add a space between asterisks, except for the last one in the row
                 if (j < x - 1) {
                     System.out.print(" "); 
                 }
             }
-
+                // Add a space at the end of even-numbered rows
             if (i % 2 == 0 && i < x - 1) {
                 System.out.print(" ");
             }
-
+            
+            // Move to the next line to start a new row
             System.out.println();
         }
     }
